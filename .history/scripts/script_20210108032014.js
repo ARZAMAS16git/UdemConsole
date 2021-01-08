ce2dea970aefb1a623ab6ab6416411d00459670a
ARZAMAS16git <alex666che@mail.ru>
2021-01-08 03:52:20 +0300

    цыклы

---
+++ .history/scripts/script_20210108032014.js
@@ -0,0 +1,23 @@
+"user strict";
+
+const numberOfFilms = +prompt (' Сколько фильмов вы уже просмотрели',"");
+
+const personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    private : false
+
+};
+
+const a = prompt('Oдин из последних просмотренных фильмов',""),
+      b = prompt("На сколько оцените его?" ,""),
+      c = prompt('Oдин из последних просмотренных фильмов',""),
+      d = prompt("На сколько оцените его?" ,"");
+      
+personalMovieDB.movies[a] = b;      
+personalMovieDB.movies[c] = d;
+for
+
+console.log(personalMovieDB);
---
+++ .history/scripts/script_20210108032221.js
@@ -0,0 +1,26 @@
+"user strict";
+
+const numberOfFilms = +prompt (' Сколько фильмов вы уже просмотрели',"");
+
+const personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    private : false
+
+};
+
+const a = prompt('Oдин из последних просмотренных фильмов',""),
+      b = prompt("На сколько оцените его?" ,""),
+      c = prompt('Oдин из последних просмотренных фильмов',""),
+      d = prompt("На сколько оцените его?" ,"");
+      
+personalMovieDB.movies[a] = b;      
+personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++){
+    const a = prompt('Oдин из последних просмотренных фильмов',""),
+        b = prompt("На сколько оцените его?" ,""); 
+}
+
+console.log(personalMovieDB);
